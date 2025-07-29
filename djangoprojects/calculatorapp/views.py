43a0f1fd2c569680f54e13503b7c07eb8f18b3aa
Luvaha <luvaha456@gmail.com>
2025-07-29 21:36:28 +0300

    Added submitquery view

--- djangoprojects/calculatorapp/views.py
+++ djangoprojects/calculatorapp/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def submitquery(request):
+    q = request.GET['query']
+    return HttpResponse(q)
